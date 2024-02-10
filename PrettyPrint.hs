6d1523b301e0d65f4480dc01496b537d4ae17b6b
Matthew Schneider <panat.matthew@gmail.com>
2024-02-10 18:10:46 -0500

    Fixing unidiomatic Haskell
    I think this should cover all instances. It seems the instances of the Pretty
    printer for the actual assembly was correct.
    
    Still need to check if it does not exhibit exponential behavior.

--- PrettyPrint.hs
+++ PrettyPrint.hs
@@ -6,12 +6,10 @@ data Doc -- the constructors are not public to the user
 
 -- monoid?
 (<>) :: Doc -> Doc -> Doc
-(<>) doc y =
-  case doc of
-    Text s x -> Text s (x Main.<> y) -- x is a Doc
-    Line i x -> Line i (x Main.<> y)
-    Nil -> y
-    Union x z -> Union (x Main.<> y) (z Main.<> y)
+(<>) (Text s x) y = Text s (x Main.<> y) -- x is a Doc
+(<>) (Line i x) y = Line i (x Main.<> y)
+(<>) Nil y = y
+(<>) (Union x z) y = Union (x Main.<> y) (z Main.<> y)
 
 -- these functions are the API (?) which makes the implementation opaque
 nil :: Doc
@@ -24,19 +22,15 @@ line :: Doc
 line = Line 0 Nil -- 0 nesting?
 
 nest :: Int -> Doc -> Doc
-nest i doc =
-  case doc of
-    Text s x -> Text s (nest i x)
-    Line j x -> Line (i + j) (nest i x)
-    Nil -> Nil
-    Union x y -> Union (nest i x) (nest i y)
+nest i (Text s x) = Text s (nest i x)
+nest i (Line j x) = Line (i + j) (nest i x)
+nest i Nil = Nil
+nest i (Union x y) = Union (nest i x) (nest i y)
 
 layout :: Doc -> String
-layout doc =
-  case doc of
-    Text s x -> s ++ layout x
-    Line i x -> '\n' : replicate i ' ' ++ layout x
-    Nil -> ""
+layout (Text s x) = s ++ layout x
+layout (Line i x) = '\n' : replicate i ' ' ++ layout x
+layout Nil = ""
 
 {-
 Given a document, representing a set of layouts, group returns the set with one
@@ -44,12 +38,10 @@ new element added, representing the layout in which everything is compressed
 on one line.
 -}
 group :: Doc -> Doc
-group doc =
-  case doc of
-    Text s x -> Text s (group x)
-    Line i x -> Union (Text " " (flatten x)) (Line i x) -- two options here
-    Nil -> Nil
-    Union x y -> Union (group x) y -- left only? an invariant I don't understand
+group (Text s x) = Text s (group x)
+group (Line i x) = Union (Text " " (flatten x)) (Line i x) -- two options here
+group Nil = Nil
+group (Union x y) = Union (group x) y -- left only? an invariant I don't understand
 
 -- w total width
 pretty :: Int -> Doc -> String
@@ -59,12 +51,10 @@ pretty w doc = layout (best w 0 doc) -- 0 = no chars on first line
 (<|>) x y = Union x y
 
 flatten :: Doc -> Doc
-flatten doc =
-  case doc of
-    Text s x -> Text s (flatten x)
-    Line i x -> Text " " (flatten x) -- same as above except no union?
-    Nil -> Nil
-    Union x y -> flatten x -- also due to the invariant
+flatten (Text s x) = Text s (flatten x)
+flatten (Line i x) = Text " " (flatten x) -- same as above except no union?
+flatten Nil = Nil
+flatten (Union x y) = flatten x -- also due to the invariant
 
 {-
 Next, it is necessary to choose the best among the set of possible layouts. This
@@ -76,15 +66,13 @@ params:
 - the number of characters k already placed on the current line
 -}
 best :: Int -> Int -> Doc -> Doc
-best w k doc =
-  case doc of
-    Nil -> Nil
-    -- for a newline it is set to the indentation
-    Line i x -> Line i (best w i x)
-    -- for text it is incremented by the string length
-    Text s x -> Text s (best w (k + length s) x)
-    -- For a union, the better of the best of the two options is selected
-    Union x y -> better w k (best w k x) (best w k y)
+best w k Nil = Nil
+-- for a newline it is set to the indentation
+best w k (Line i x) = Line i (best w i x)
+-- for text it is incremented by the string length
+best w k (Text s x) = Text s (best w (k + length s) x)
+-- For a union, the better of the best of the two options is selected
+best w k (Union x y) = better w k (best w k x) (best w k y)
 
 better :: Int -> Int -> Doc -> Doc -> Doc
 better w k x y =
@@ -93,15 +81,13 @@ better w k x y =
     else y
 
 fits :: Int -> Doc -> Bool
-fits w doc =
-  if w < 0 -- no available width remaining for anything else
-    then False
-    else case doc of
-           Nil -> True
-           -- if the document begins with text then it fits if the remaining
-           -- document fits in the remaining space
-           Text s x -> fits (w - length s) x
-           Line i x -> True -- always true?... document begins with newline!
+fits w doc
+  | w < 0 = False
+fits w Nil = True
+-- if the document begins with text then it fits if the remaining
+-- document fits in the remaining space
+fits w (Text s x) = fits (w - length s) x
+fits w (Line i x) = True -- always true?... document begins with newline!
 
 {-
 TREE EXAMPLE!!
