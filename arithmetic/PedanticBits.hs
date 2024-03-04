76ae49726d1de9e2c66d7bee09c2273ba67eb1e7
Matthew Schneider <panat.matthew@gmail.com>
2024-03-04 18:48:03 -0500

    A way to reason with bit types

--- arithmetic/PedanticBits.hs
+++ arithmetic/PedanticBits.hs
@@ -12,10 +12,12 @@
       | e | e
       | e ^ e
       | e + e
+      | let x = e in e
+      | x
 -}
 data Expr
   = Lit Int
-  | BinOp BinOp Expr Expr
+  | BinaryOp BinaryOp Expr Expr
   | UnaryOp UnaryOp Expr
   | Let Id Type Expr Expr
   | Var Id
@@ -28,9 +30,9 @@ data UnaryOp
   | Shrink
   deriving (Show)
 
-data BinOp
-  = BitOp
-  | ArithOp
+data BinaryOp
+  = BitOp BitOp
+  | ArithOp ArithOp
   deriving (Show)
 
 data BitOp
@@ -53,19 +55,29 @@ type Id = String
 type Context = [(Id, Type)]
 
 data Type =
-  BitType BitParam
-  deriving (Show)
+  BitType Word (Bound Word) -- lower bound, upper bound
 
-data BitParam
-  = Exactly Word -- = n
-  | AtLeast Word -- >= n
-  | AtMost Word -- <= n
-  deriving (Show)
+instance Show Type where
+  show (BitType m (Finite n))
+    | m == n = "bit[" ++ show n ++ "]"
+    | otherwise = "bit[n] where " ++ show m ++ "<=n<=" ++ show n
+  show (BitType m Infinity) = "bit[n] where n>=" ++ show m
+
+data Bound a -- this is just the Maybe type, but with cool names
+  = Finite a
+  | Infinity
+  deriving (Show, Eq)
+
+instance (Ord a) => Ord (Bound a) where
+  (<=) (Finite _) Infinity = True
+  (<=) (Finite a) (Finite b) = a <= b
+  (<=) Infinity _ = False
+  (<=) Infinity Infinity = True
 
 -- relation from TAPL
 -- T1 <: T2 if and only if T1 is compatible with T2
 (<:) :: Type -> Type -> Bool
-(<:) (BitType (Exactly n)) (BitType (Exactly m)) = n == m
+--(<:) (BitType (Exactly n)) (BitType (Exactly m)) = n == m
 (<:) _ _ = undefined
 
 extend :: Context -> Id -> Type -> Context
@@ -76,15 +88,55 @@ typeof = typeof' []
 
 typeof' :: Context -> Expr -> Type
 typeof' _ (Lit i)
-  | i > 0 = BitType (Exactly (ceiling $ logBase 2 $ (fromIntegral i) + 1))
-  | i == 0 = BitType (Exactly 0)
-  | i < 0 = BitType (Exactly (ceiling $ logBase 2 $ (abs $ fromIntegral i)))
+  | i > 0 =
+    let bits = ceiling $ logBase 2 $ (fromIntegral i) + 1
+     in constant bits
+  | i == 0 = constant 0
+  | i < 0 =
+    let bits = ceiling $ logBase 2 $ (abs $ fromIntegral i)
+     in constant bits
+  where
+    constant x = BitType x (Finite x)
 typeof' c (UnaryOp BitNot e) = typeof' c e
+{-
+  extend constant   [n,n]   => [n,inf)
+  shrink bounded    [m,n]   => [m,inf)
+  extend lower bnd  [n,inf) => [n,inf)
+  extend upper bnd  [0,n]   => [0,inf)
+  extend unbounded  [0,inf) => [0,inf)
+-}
 typeof' c (UnaryOp Extend e) =
   case typeof' c e of
-    BitType (Exactly n) -> BitType (AtLeast n)
-    BitType (AtLeast n) -> BitType (AtMost n)
-typeof' c (UnaryOp SignExtend e) = typeof' c e
-typeof' c (UnaryOp BitNot e) = typeof' c e
-typeof' _ _ = undefined
--- stuck, giving up for now
+    BitType lb _ -> BitType lb Infinity
+typeof' c (UnaryOp SignExtend e) =
+  case typeof' c e of
+    BitType lb _ -> BitType lb Infinity
+{-
+  shrink constant   [n,n]   => [0,n]
+  shrink bounded    [m,n]   => [0,n]
+  shrink lower bnd  [n,inf) => [0,inf)
+  shrink upper bnd  [0,n]   => [0,n]
+  shrink unbounded  [0,inf) => [0,inf)
+-}
+typeof' c (UnaryOp Shrink e) =
+  case typeof' c e of
+    BitType _ ub -> BitType 0 ub
+{-
+  binary operator typing rules
+  * the general idea: the sizes should match!
+  * compute the intersection of the possible ranges of the types
+  * the size of the type is the intersection of the ranges
+  * it will fail to typecheck if the sizes are disjoint!
+-}
+typeof' c (BinaryOp _ e1 e2) =
+  let (t1, t2) = (typeof' c e1, typeof' c e2)
+   in intersect t1 t2
+  where
+    intersect (BitType lb1 ub1) (BitType lb2 ub2) =
+      if (Finite lb2) > ub1 || (Finite lb1) > ub2
+        then error "Incompatible types in binary operation"
+        else BitType (max lb1 lb2) (min ub1 ub2)
+
+example :: Expr
+example =
+  BinaryOp (BitOp BitAnd) (UnaryOp Extend (Lit 5)) (UnaryOp Shrink (Lit 50))
